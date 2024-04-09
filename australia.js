2021415c37d87430d3f55b28a31034feb7f58a7f
Elisa <elibeltramone@gmail.com>
2024-04-09 06:36:20 +0200

    conversor

--- australia.js
+++ australia.js
@@ -1,7 +1,6 @@
-// Array de nombres de los estados de Australia
+// Estados de Australia
 const estados = ['Western', 'Queensland', 'Victoria', 'Northern', 'New', 'Tasmania', 'South'];
 
-// Recorre cada estado y agrega los eventos hover
 estados.forEach(estado => {
     const map = document.getElementById(`map${estado}`);
     const contenedor = document.getElementById(`card${estado}`);
@@ -24,4 +23,28 @@ estados.forEach(estado => {
 
 });
 
+// conversor divisas
+document.getElementById('currency-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    const amount = document.getElementById('amount').value;
+    const fromCurrency = document.getElementById('from').value;
+    const toCurrency = document.getElementById('to').value;
+
+    const url = `https://api.exchangeratesapi.io/latest?base=${fromCurrency}&symbols=${toCurrency}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.rates[toCurrency]) {
+                const result = (amount * data.rates[toCurrency]).toFixed(2);
+                document.getElementById('result-container').textContent = `Resultado: ${result} ${toCurrency}`;
+            } else {
+                document.getElementById('result-container').textContent = 'No se encontró la tasa de cambio para la moneda de destino.';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('result-container').textContent = 'Error al convertir la divisa. Por favor, inténtalo de nuevo.';
+        });
+});
